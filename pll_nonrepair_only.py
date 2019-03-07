de5b7d3ed57b3af7f6bbd8ca12ca21e8f7838bc3
Tiep <ngohongtiep@gmail.com>
2019-03-07 16:21:10 +0700

    Update PLL MBIST group 15 (new) - group 6 7 10

--- pll_nonrepair_only.py
+++ pll_nonrepair_only.py
@@ -7,6 +7,7 @@ icl = pd.read_csv('icl.csv', index_col='icl', delimiter=",")
 #controller = pd.read_csv('controller.csv', index_col='controller', delimiter=",")
 block = "zx222016"
 #groups = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
+groups = ["6", "7", "10", "15"]
 full_spec = ""
 #full_controller = pd.merge(data.reset_index(),controller.reset_index(), on="block", how="outer").set_index(['controller', 'icl'])
 #full_controller = pd.merge(data.reset_index(),controller.reset_index(), on="block", how="outer")
