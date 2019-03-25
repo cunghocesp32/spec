9cd25079b9a873804869bc58e4cc5b2dcc50738c
Tiep <ngohongtiep@gmail.com>
2019-03-25 10:13:22 +0700

    New 15 groups MBIST PLL BP for IR drop (190325)

--- pll_nonrepair_only.py
+++ pll_nonrepair_only.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import jinja2
 
-
-
 icl = pd.read_csv('icl.csv', index_col='icl', delimiter=",")
 #controller = pd.read_csv('controller.csv', index_col='controller', delimiter=",")
 block = "zx222016"
-#groups = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
-groups = ["6", "7", "10", "15"]
-groups = ["4", "16"]
+groups = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"]
+#groups = ["6", "7", "10", "15"]
+#groups = ["4", "16"]
 full_spec = ""
 #full_controller = pd.merge(data.reset_index(),controller.reset_index(), on="block", how="outer").set_index(['controller', 'icl'])
 #full_controller = pd.merge(data.reset_index(),controller.reset_index(), on="block", how="outer")
